d00955197e1a05dbefc7cc16d15b3526f14f4a28
Kian_Breslin <kianbreslin517@gmail.com>
2024-05-20 18:09:28 +0100

    Initial Commit

---
+++ BusyBuilders/BusyBuildersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusyBuildersApp.swift
+//  BusyBuilders
+//
+//  Created by Kian Breslin on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BusyBuildersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
