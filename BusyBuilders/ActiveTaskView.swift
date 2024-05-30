b192dfa04ff7079a9a4a2ca2baefb6d3f13ad481
Kian_Breslin <kianbreslin517@gmail.com>
2024-05-31 00:43:15 +0200

    [Feature] Desinged and integrated Active task View. Added Timer function

--- BusyBuilders/ActiveTaskView.swift
+++ BusyBuilders/ActiveTaskView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import SwiftData
 
 struct ActiveTaskView: View {
+    
+    @Environment(\.modelContext) var context
+    @Query var businesses : [BusinessDataModel]
 
     @State var timeRemaining : Int
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -18,10 +21,102 @@ struct ActiveTaskView: View {
     
     var body: some View {
         ZStack {
-            Color.red
+            Color(red: 0.2, green: 0.2, blue: 0.2)
                 .ignoresSafeArea()
-            Text("Time Remaining: \(timeFormatted(timeRemaining))")
-                .font(.system(size: 40))
+            VStack {
+                
+                ZStack {
+                    Circle()
+                        .frame(width: 205, height: 205)
+                        .foregroundStyle(.red)
+                    Circle()
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color(red: 0.1, green: 0.1, blue: 0.1))
+                        .overlay {
+                            Image("store")
+                                .resizable()
+                        }
+                }
+                
+                RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                    .frame(width: 370, height: 50)
+                    .foregroundStyle(Color(red: 36/255, green: 36/255, blue: 36/255))
+                    .overlay {
+//                        Text("\(businesses[0].businessName)")
+//                            .foregroundStyle(.white)
+//                            .font(.title)
+                    }
+                
+                HStack {
+                    RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                        .frame(width: 180, height: 50)
+                        .foregroundStyle(Color(red: 36/255, green: 36/255, blue: 36/255))
+                        .overlay {
+                            Text("Value : $300,000")
+                        }
+                    
+                    RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                        .frame(width: 180, height: 50)
+                        .foregroundStyle(Color(red: 36/255, green: 36/255, blue: 36/255))
+                        .overlay {
+                            Text("$ / Min : $500")
+                        }
+                }
+                .foregroundStyle(.white)
+                .font(.title3)
+                .padding(.horizontal, 10)
+                
+                VStack {
+                    Text("Time Remaining")
+                        .foregroundStyle(.white)
+                        .opacity(0.4)
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 370, height: 80)
+                        .foregroundStyle(Color(red: 36/255, green: 36/255, blue: 36/255))
+                        .overlay {
+                            Text("\(timeFormatted(timeRemaining))")
+                                .font(.system(size: 40))
+                                .foregroundStyle(.white)
+                        }
+                }
+                
+                VStack {
+                    Text("Next Level")
+                        .foregroundStyle(.white)
+                        .opacity(0.4)
+                    
+                    HStack {
+                        Text("18")
+                            .frame(width: 35, height: 40)
+                        ZStack (alignment: .leading){
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 300, height: 25)
+                                .foregroundStyle(Color(red: 36/255, green: 36/255, blue: 36/255))
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 200, height: 20)
+                                .foregroundStyle(.red)
+                        }
+                        Text("19")
+                            .frame(width: 35, height: 40)
+                    }
+                }
+                
+                VStack {
+                    Text("Total Earnings")
+                        .foregroundStyle(.white)
+                        .opacity(0.4)
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 300, height: 50)
+                        .foregroundStyle(Color(red: 36/255, green: 36/255, blue: 36/255))
+                        .overlay {
+                            Text("$6,000")
+                                .font(.title)
+                                .foregroundStyle(.white)
+                        }
+                }
+            }
         }
         .onReceive(timer) { time in
             guard isActive else {return}
@@ -49,5 +144,5 @@ struct ActiveTaskView: View {
 
 #Preview {
     ActiveTaskView(timeRemaining: 3600)
-        .modelContainer(for: [BusinessDataModel.self], inMemory: true)
+        .modelContainer(for: [BusinessDataModel.self])
 }
