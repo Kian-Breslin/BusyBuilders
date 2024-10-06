2bf995d535e7759b9e31b661390c7e0d73cf3921
Kian_Breslin <kianbreslin517@gmail.com>
2024-10-06 13:11:16 -0700

    Task Update
    Back-End:
    N/A
    
    Dashboard :
    N/A
    
    Communities :
    N/A
    
    Play :
    - Can now see how much you earned from the task
    
    Messages :
    N/A
    
    Portfolio :
    N/A
    
    Other :
    - Added a Modal view folder for files that are SubMainViews
    - Deleted DataViews folder
    - Added DevTool to add RandomName businesses

---
+++ BusyBuilders/ModalViews/PostTask.swift
@@ -0,0 +1,18 @@
+//
+//  PostTask.swift
+//  BusyBuilders
+//
+//  Created by Kian Breslin on 06/10/2024.
+//
+
+import SwiftUI
+
+struct PostTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostTask()
+}
